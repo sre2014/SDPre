af1977724dfdfe0316cc6c267f85529950eb7685
sre2014 <workuse4129@gmail.com>
2025-05-15 22:34:27 -0600

    complete Objective2Lab3

---
+++ Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    // Your solution goes here
+    int birthYear = 1978;
+    String birthMonth = "June";
+    boolean isWeekend = false;
+
+    System.out.println("Year of Birth: " + birthYear);
+    System.out.println("Month of Birth: " + birthMonth);
+    System.out.println("Is today a weekend? " + isWeekend);
+  }
+}
