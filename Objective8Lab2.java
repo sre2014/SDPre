bf8e6ceaa6f2b1d0b7684f3c07ac7c2660bb1ac9
sre2014 <workuse4129@gmail.com>
2025-05-16 02:13:34 -0600

    complete Objective8Lab2

---
+++ Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0; // Variable to store the current sum
+
+    for(int i = 1; i <= 20; i++) {
+      sum += i; // Add the current number to the sum
+    }
+
+    System.out.println("The sum of numbers from 1 to 20 is: " + sum);
+  }
+}
