fb43a7bfd321fd48a00d1cfd17f9a109612fe9b1
sre2014 <workuse4129@gmail.com>
2025-05-16 01:47:54 -0600

    complete Objective7Lab2

---
+++ Objective7Lab2.java
@@ -0,0 +1,13 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while(counter <= 20) {
+      // Print the value of counter in each iteration
+      System.out.println(counter);
+
+      // Add 1 to counter each iteration
+      counter = counter + 1;
+    }
+  }
+}
