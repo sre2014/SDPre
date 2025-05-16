062d4b870f0ab632da57f1d085de06f2008c6e4c
sre2014 <workuse4129@gmail.com>
2025-05-15 22:54:17 -0600

    complete Objective3Lab1

---
+++ Objective3Lab1.java
@@ -0,0 +1,14 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    // Your solution goes here
+    num1 = 10.5;
+    num2 = 20.0;
+    num3 = 15.5;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println("The average is: " + average);
+  }
+}
