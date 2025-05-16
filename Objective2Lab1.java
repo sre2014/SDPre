74f847dc0cdf1f0b760acdfdc6ae23e19ff2bf05
sre2014 <workuse4129@gmail.com>
2025-05-15 22:30:57 -0600

    complete Objective2Lab1

---
+++ Objective2Lab1.java
@@ -0,0 +1,8 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+
+    // Your solution goes here
+    System.out.println(hello);
+  }
+}
