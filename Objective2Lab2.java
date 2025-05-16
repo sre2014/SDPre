8b7efdf000cc4ded2b9bdca003de16cd30a9add0
sre2014 <workuse4129@gmail.com>
2025-05-15 22:32:40 -0600

    complete Objective2Lab2

---
+++ Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    // Your solution goes here
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("State: " + state);
+  }
+}
