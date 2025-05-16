7b7cf76d8b14d9d78bd90d436d6e233ab3ae65e3
sre2014 <workuse4129@gmail.com>
2025-05-16 01:42:17 -0600

    complete Objective7Lab1

---
+++ Objective7Lab1.java
@@ -0,0 +1,13 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+      // Print the value of counter in each iteration
+      System.out.println(counter);
+
+      // Decrease the counter by 1 after printing
+      counter = counter - 1; 
+    }
+  }
+}
